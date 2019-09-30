9ffb32dd7caf2cdb42a9742d62645d11c1f155fa
vaibhaviiit <vaibhav.kumar@truebil.com>
2019-09-30 17:52:06 +0530

    Added design mode

--- background.js
+++ background.js
@@ -8,5 +8,5 @@ function clickedIcon(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
+  })
+}
--- content.js
+++ content.js
@@ -2,12 +2,13 @@
 chrome.runtime.onMessage.addListener(iconClickedHandler);
 
 function iconClickedHandler(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      const isDesignModeOn = document.designMode === "on";
-      if (isDesignModeOn) {
-        document.designMode = "off";
-      } else {
-        document.designMode = "on";
-      }
+  if (request.message === "clicked_browser_action") {
+    const isDesignModeOn = document.designMode === "on";
+    if (isDesignModeOn) {
+      document.designMode = "off";
+    } else {
+      document.designMode = "on";
     }
+    console.log("Design Mode " + document.designMode);
   }
+}
