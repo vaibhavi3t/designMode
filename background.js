c39fde41aa3b4fb0bb7a9e7a6e60b1cd34846b46
vaibhaviiit <vaibhav.kumar@truebil.com>
2019-09-30 17:45:58 +0530

    refactored code

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(clickedIcon);
+
+function clickedIcon(tab) {
   // Send a message to the active tab
-  console.log(tab)
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    console.log(tabs)
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
--- content.js
+++ content.js
@@ -1,15 +1,13 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(iconClickedHandler);
+
+function iconClickedHandler(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       const isDesignModeOn = document.designMode === "on";
       if (isDesignModeOn) {
         document.designMode = "off";
-        chrome.browserAction.setIcon({path: "icon-no-edit.png"});
       } else {
         document.designMode = "on";
-        chrome.browserAction.setIcon({path: "icon-edit.png"});
       }
     }
   }
-);
